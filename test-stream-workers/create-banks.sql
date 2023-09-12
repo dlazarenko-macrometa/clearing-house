c657878768487d104600fcbf346b548e6aeec239
Yehan Pemarathne <86756694+yehan-macrometa@users.noreply.github.com>
2023-09-12 11:58:33 +0530

    create-banks: Add _key

--- test-stream-workers/create-banks.sql
+++ test-stream-workers/create-banks.sql
@@ -3,7 +3,7 @@
 
 CREATE TRIGGER MyTrigger WITH ( interval = 1 sec );
 
-CREATE STORE Banks WITH (type='database', replication.type="global", collection.type="doc") (uuid string, name string,  balance long, reserved long, currency string, region string);
+CREATE STORE Banks WITH (type='database', replication.type="global", collection.type="doc") (_key string, uuid string, name string,  balance long, reserved long, currency string, region string);
 
 INSERT INTO countstream 
 SELECT count() as c 
@@ -11,5 +11,5 @@ from MyTrigger;
 
 -- 'eventTimestamp()' returns the timestamp of the processed/passed event.
 INSERT INTO Banks
-SELECT convert(c % 100, 'string') as uuid, str:concat("Chase_", convert(c % 100, 'string')) as name, 10000L as balance, 0L as reserved, "USD" as currency, "dmytro-us-west" as region
+SELECT str:concat("Chase_", convert(c % 24000, 'string')) as _key, convert(c % 24000, 'string') as uuid, str:concat("Chase_", convert(c % 24000, 'string')) as name, 10000L as balance, 0L as reserved, "USD" as currency, "ch64mn-us-west" as region
 FROM countstream;
